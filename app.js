c434358b41e6d3aad7a38a013a3cc87b2335389a
Toshi <waykiyoharu@gmail.com>
2020-08-25 20:44:12 +0900

    fileuplodの準備

--- app.js
+++ app.js
@@ -2,14 +2,20 @@
 const express = require('express');
 const app = express();
 
+//fileuploadの準備
+const upload = require('express-fileupload')
+
 
 //publicフォルダ内のcssや画像フォルダの読み取りを可能にする
 app.use(express.static('public'));
 
 
+app.use(upload());
+
+
 //ルーティングを定義
 app.get('/',(req, res)=>{
-    res.render('top.ejs')
+    res.sendFile(__dirname +'/index.html')
 });
 
 
