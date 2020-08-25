acdb99452f8f0dba785fe0bdf8e931f097a42c99
Toshi <waykiyoharu@gmail.com>
2020-08-25 19:43:48 +0900

    add new file

---
+++ app.js
@@ -0,0 +1,19 @@
+//expressの準備
+const express = require('express');
+const app = express();
+
+
+//publicフォルダ内のcssや画像フォルダの読み取りを可能にする
+app.use(express.static('public'));
+
+
+//ルーティングを定義
+app.get('/',(req, res)=>{
+    //res.sendで簡易的に文字表示
+    // res.send('hello');
+    res.render('top.ejs')
+});
+
+
+//ポートの読み込み
+app.listen(3003);
